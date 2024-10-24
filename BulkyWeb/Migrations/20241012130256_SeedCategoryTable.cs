35f640b36a89f2208379a9534e6c1b7dd537f3d0
Stefan Adlertz <stefan@sa-education.com>
2024-10-25 00:34:51 +0200

    git commit -m "Initial commit after reinitializing repository"

---
+++ BulkyWeb/Migrations/20241012130256_SeedCategoryTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "SciFi");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "SciFI");
+        }
+    }
+}
